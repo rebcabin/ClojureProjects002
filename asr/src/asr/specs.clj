aab4732bca9729e3bc0bd4c7ea3f6ecba5e574a6
rebcabin <rebcabin@users.noreply.github.com>
2022-10-23 06:40:01 -0700

    :asr.specs/ttype now generating

--- asr/src/asr/specs.clj
+++ asr/src/asr/specs.clj
@@ -2,12 +2,23 @@
   (:use [asr.utils]
         [asr.data]
         [asr.numbers]
-        [asr.base-specs]
+        [asr.base-specs]  ; defines :asr.specs/identifier
         [asr.autospecs])
   (:require [clojure.spec.alpha            :as s   ]
             [clojure.zip                   :as z   ]
             [clojure.spec.gen.alpha        :as gen ]
             [clojure.test.check.generators :as tgen]))
+
+
+;;  _    _         _   _  __ _
+;; (_)__| |___ _ _| |_(_)/ _(_)___ _ _
+;; | / _` / -_) ' \  _| |  _| / -_) '_|
+;; |_\__,_\___|_||_\__|_|_| |_\___|_|
+
+;;; To break a cycle of imports, :asr.specs/identifier is
+;;; specified in asr.base-specs. The definition of that spec
+;;; writes into this namespace, :asr.specs, from another
+;;; namespace, :asr.base-specs.
 
 
 ;;     _ _                   _
@@ -191,26 +202,8 @@
 ;;      (Character 1 [1])
 ;;      (Real 4 [6 482188844534185])
 ;;      (Logical 8 [115])))
-;; => ((Tuple)
-;;     (Tuple)
-;;     (Tuple (Character 1 [18]))
-;;     (Tuple (Character 1 []) (Integer 2 []) (Real 4 []))
-;;     (Tuple)
-;;     (Tuple (Real 8 [366308]))
-;;     (Tuple (Real 4 []))
-;;     (Tuple (Real 4 [55]) (Complex 4 [1]))
-;;     (Tuple (Integer 8 []) (Character 1 [2]) (Logical 4 [1156383710447]))
-;;     (Tuple
-;;      (Character 1 [])
-;;      (Complex 4 [1])
-;;      (Complex 8 [897 0])
-;;      (Logical 1 [339410184938])
-;;      (Real 4 [5])
-;;      (Complex 8 [1 58331])
-;;      (Logical 8 [])
-;;      (Logical 8 [16890081 241])
-;;      (Logical 1 [263663 1324679404525])))
 
+;;; Filling this out as we go along
 
 (s/def ::ttype
   (s/or :integer   ::integer-ttype
@@ -223,18 +216,7 @@
         :tuple     ::tuple-ttype
         ))
 
-
 (gen/sample (s/gen ::ttype))
-;; => ((Integer 1 [])
-;;     (Set (Set (Logical 4 [0])))
-;;     (Logical 1 [])
-;;     (Set (Set (Real 8 [0 3025])))
-;;     (Set (Complex 4 [1 576454]))
-;;     (Real 8 [5])
-;;     (Set (Integer 2 [516997 15]))
-;;     (Complex 4 [14])
-;;     (Integer 8 [66 288322])
-;;     (Logical 1 [2 9]))
 ;; => ((Complex 4 [1999])
 ;;     (List (Set (Tuple (List (Real 4 [0])))))
 ;;     (Real 4 [2])
@@ -273,20 +255,39 @@
 ;;     (Logical 1 [2 23]))
 
 
-;;     _         _            _   _
-;;  __| |___ _ _(_)_ _____ __| | | |_ _  _ _ __  ___
-;; / _` / -_) '_| \ V / -_) _` | |  _| || | '_ \/ -_)
-;; \__,_\___|_| |_|\_/\___\__,_|  \__|\_, | .__/\___|
-;;                                    |__/|_|
+;;                   __        ___      __        _             __
+;;   ___ __ ____ _  / /  ___  / (_) ___/ /__ ____(_)  _____ ___/ /
+;;  (_-</ // /  ' \/ _ \/ _ \/ /   / _  / -_) __/ / |/ / -_) _  /
+;; /___/\_, /_/_/_/_.__/\___/_(_)  \_,_/\__/_/ /_/|___/\__/\_,_/
+;;     /___/
+;;   __
+;;  / /___ _____  ___
+;; / __/ // / _ \/ -_)
+;; \__/\_, / .__/\__/
+;;    /___/_/
 
-;;; NOTE: this is NOT derived-ttype [sic, two t's], rather
-;;; derived-type is a HEAD, DerivedType, kebabulated, of the TERM
-;;; symbol. However, derived-ttype uses derived-type.
+;;; NOTE: this is NOT derived-ttype [sic, two t's], rather, this
+;;; derived-type is a symbol, specifically, the HEAD, DerivedType,
+;;; kebabulated, of the TERM, symbol. However, derived-ttype uses
+;;; derived-type.
+
+;;; The question: whether sometimes "symbol_table" means a literal
+;;; symbol table and sometimes means a numerical id of or
+;;; reference to a symbol table. In the case of Variable, the type
+;;; "symbol_table" definitely means "numerical ID of a symbol
+;;; table that's reified somewhere else." The example in data.clj
+;;; shows that to be the case. Perhaps we can conclude that
+;;; whenever the _name_ of a variable of type symbol_table
+;;; is "parent_symtab," then the value of the variable is actually
+;;; of type positive uint. TODO: this isn't great practice. The
+;;; type of a variable, not the name of a variable, should typify
+;;; the value of the variable.
+;;; https://github.com/lcompilers/lpython/issues/1224
 
 (s/def ::derived-type
   (s/cat
    :head         #{'DerivedType}
-   :symbol-table #{'placeholder-symbol-table}
+   :symbol-table #{'symbol-table-placeholder}
    :nym          ::identifier
    :members      (s/* ::identifier)
    :abi          :asr.autospecs/abi
@@ -305,16 +306,388 @@
 ;;       :parent [:derived-type D]}])
 
 
+;;      __        _             __  __  __
+;;  ___/ /__ ____(_)  _____ ___/ / / /_/ /___ _____  ___
+;; / _  / -_) __/ / |/ / -_) _  / / __/ __/ // / _ \/ -_)
+;; \_,_/\__/_/ /_/|___/\__/\_,_/  \__/\__/\_, / .__/\__/
+;;                                       /___/_/
+
 (s/def ::derived-ttype
   (s/cat
    :head         #{'Derived}
-   :derived-type ::identifier
+   :derived-type ::derived-type
    :dims         ::dimensions))
 
 (s/exercise ::derived-ttype 1)
 ;; => ([(Derived c [957])
 ;;      {:head Derived, :derived-type c, :dims [[:bigint 957]]}])
 
+
+;;                   __        ___                         __
+;;   ___ __ ____ _  / /  ___  / (_) ___ ___  __ ____ _    / /___ _____  ___
+;;  (_-</ // /  ' \/ _ \/ _ \/ /   / -_) _ \/ // /  ' \  / __/ // / _ \/ -_)
+;; /___/\_, /_/_/_/_.__/\___/_(_)  \__/_//_/\_,_/_/_/_/  \__/\_, / .__/\__/
+;;     /___/                                                /___/_/
+
+;;; Again, NOT enum ttype; we'll get there in a minute.
+
+;; EnumType(
+;;   symbol_table symtab,
+;;   identifier   name,
+;;   identifier*  members,
+;;   abi          abi,
+;;   access       access,
+;;   ttype        type,
+;;   symbol?      parent)
+
+(s/def ::enum-type
+  (s/cat
+   :head         #{'EnumType}
+   :symbol-table #{'symbol-table-placeholder}
+   :nym          ::identifier
+   :members      (s/* ::identifier)
+   :abi          :asr.autospecs/abi
+   :access       :asr.autospecs/access
+   :type         ::ttype
+   :parent       (s/or :derived-type ::identifier
+                       :class-type   ::identifier)
+   ))
+
+
+;;                         __  __
+;;  ___ ___  __ ____ _    / /_/ /___ _____  ___
+;; / -_) _ \/ // /  ' \  / __/ __/ // / _ \/ -_)
+;; \__/_//_/\_,_/_/_/_/  \__/\__/\_, / .__/\__/
+;;                              /___/_/
+
+(s/def ::enum-ttype
+  (s/cat
+   :head       #{'Enum}
+   :enum-type  ::enum-type
+   :dims       ::dimensions))
+
+(s/exercise ::enum-ttype 1)
+;; => ([(Enum b []) {:head Enum, :enum-type b, :dims []}])
+
+
+;;                   __        ___       __               __
+;;   ___ __ ____ _  / /  ___  / (_) ____/ /__ ____ ___   / /___ _____  ___
+;;  (_-</ // /  ' \/ _ \/ _ \/ /   / __/ / _ `(_-<(_-<  / __/ // / _ \/ -_)
+;; /___/\_, /_/_/_/_.__/\___/_(_)  \__/_/\_,_/___/___/  \__/\_, / .__/\__/
+;;     /___/                                               /___/_/
+
+;;; ClassType(
+;;;   symbol_table symtab,
+;;;   identifier name,
+;;;   abi abi,
+;;;   access access)
+
+(s/def ::class-type
+  (s/cat
+   :head    #{'ClassType}
+   :nym     ::identifier
+   :abi     :asr.autospecs/abi
+   :access  :asr.autospecs/access
+   ))
+
+
+;;       __               __  __
+;;  ____/ /__ ____ ___   / /_/ /___ _____  ___
+;; / __/ / _ `(_-<(_-<  / __/ __/ // / _ \/ -_)
+;; \__/_/\_,_/___/___/  \__/\__/\_, / .__/\__/
+;;                             /___/_/
+
+(s/def ::class-ttype
+  (s/cat
+   :head       #{'Class}
+   :class-type ::class-type
+   :dims       ::dimensions))
+
+
+;;      ___     __    __  __
+;;  ___/ (_)___/ /_  / /_/ /___ _____  ___
+;; / _  / / __/ __/ / __/ __/ // / _ \/ -_)
+;; \_,_/_/\__/\__/  \__/\__/\_, / .__/\__/
+;;                         /___/_/
+
+(s/def ::dict-ttype
+  (s/cat
+   :head       #{'Dict}
+   :key-type   ::ttype
+   :value-type ::ttype))
+
+
+;;               _      __            __  __
+;;    ___  ___  (_)__  / /____ ____  / /_/ /___ _____  ___
+;;   / _ \/ _ \/ / _ \/ __/ -_) __/ / __/ __/ // / _ \/ -_)
+;;  / .__/\___/_/_//_/\__/\__/_/    \__/\__/\_, / .__/\__/
+;; /_/                                     /___/_/
+
+
+(s/def ::pointer-ttype
+  (s/cat
+   :head  #{'Pointer}
+   :type  ::ttype))
+
+
+;;            __        __  __
+;;  _______  / /_____  / /_/ /___ _____  ___
+;; / __/ _ \/ __/ __/ / __/ __/ // / _ \/ -_)
+;; \__/ .__/\__/_/    \__/\__/\_, / .__/\__/
+;;   /_/                     /___/_/
+
+(s/def ::cptr-ttype
+  (s/cat
+   :head #{'CPtr}))
+
+
+;;   __                                               __
+;;  / /___ _____  ___   ___  ___ ________ ___ _  ___ / /____ ____
+;; / __/ // / _ \/ -_) / _ \/ _ `/ __/ _ `/  ' \/ -_) __/ -_) __/
+;; \__/\_, / .__/\__/ / .__/\_,_/_/  \_,_/_/_/_/\__/\__/\__/_/
+;;    /___/_/        /_/
+
+(s/def ::restriction
+  (s/cat
+   :head #{'Restrction}
+   :rt   :asr.autospecs/trait))
+
+(s/def ::type-parameter-ttype
+  (s/cat
+   :head #{'TypeParameter}
+   :dims ::dimensions
+   :rt   (s/* ::restriction)
+   ))
+;;  _   _                               _      __
+;; | |_| |_ _  _ _ __  ___   _ _ ___ __| |___ / _|
+;; |  _|  _| || | '_ \/ -_) | '_/ -_) _` / -_)  _|
+;;  \__|\__|\_, | .__/\___| |_| \___\__,_\___|_|
+;;          |__/|_|
+
+;;; Filling this out as we go along
+
+(s/def ::ttype
+  (s/or :integer        ::integer-ttype
+        :real           ::real-ttype
+        :complex        ::complex-ttype
+        :character      ::character-ttype
+        :logical        ::logical-ttype
+        :set            ::set-ttype
+        :list           ::list-ttype
+        :tuple          ::tuple-ttype
+        :derived        ::derived-ttype
+        :enum           ::enum-ttype
+        :class          ::class-ttype
+        :dict           ::dict-ttype
+        :pointer        ::pointer-ttype
+        :cptr           ::cptr-ttype
+        :type-parameter ::type-parameter-ttype
+        ))
+
+;;; Here's the old, inadequate stuff:
+
+(s/exercise :asr.autospecs/ttype)
+;; => ([(List) (List)]
+;;     [(List) (List)]
+;;     [(Set) (Set)]
+;;     [(Tuple u) (Tuple u)]
+;;     [(Derived F35 x96m T) (Derived F35 x96m T)]
+;;     [(CPtr GCTvV1) (CPtr GCTvV1)]
+;;     [(Class) (Class)]
+;;     [(Real Ir RK7XtLH j4 AG MA k7ZZyKU8 Phr)
+;;      (Real Ir RK7XtLH j4 AG MA k7ZZyKU8 Phr)]
+;;     [(Complex KFr H11qG Hk5Kn77 P4n25cl7u Ju)
+;;      (Complex KFr H11qG Hk5Kn77 P4n25cl7u Ju)]
+;;     [(Real ml9oHevc2) (Real ml9oHevc2)])
+
+;;; Here's the new, fixed-up stuff:
+
+(s/exercise ::ttype)
+;; => ([(Dict (Integer 2 []) (Class ClassType t Source Private []))
+;;      [:dict
+;;       {:head Dict,
+;;        :key-type [:integer {:head Integer, :kind 2, :dims []}],
+;;        :value-type
+;;        [:class
+;;         {:head Class,
+;;          :class-type
+;;          {:head ClassType, :nym t, :abi Source, :access Private},
+;;          :dims []}]}]]
+;;     [(Set (Pointer (TypeParameter [1 0])))
+;;      [:set
+;;       {:head Set,
+;;        :ttype
+;;        [:pointer
+;;         {:head Pointer,
+;;          :type
+;;          [:type-parameter
+;;           {:head TypeParameter,
+;;            :dims [[:nat-int 1] [:nat-int 0]]}]}]}]]
+;;     [(Set (Character 1 []))
+;;      [:set
+;;       {:head Set,
+;;        :ttype [:character {:head Character, :kind 1, :dims []}]}]]
+;;     [(Real 8 []) [:real {:head Real, :kind 8, :dims []}]]
+;;     [(Tuple
+;;       (Derived
+;;        DerivedType
+;;        symbol-table-placeholder
+;;        y99Jg
+;;        eDC
+;;        L3
+;;        Dn
+;;        Intrinsic
+;;        Private
+;;        ANdva
+;;        [2675])
+;;       (List
+;;        (Set
+;;         (Tuple
+;;          (Enum
+;;           EnumType
+;;           symbol-table-placeholder
+;;           so
+;;           G1tlT
+;;           bKOn9
+;;           GS8
+;;           BindC
+;;           Private
+;;           (Dict
+;;            (Derived
+;;             DerivedType
+;;             symbol-table-placeholder
+;;             o
+;;             GFortranModule
+;;             Private
+;;             n5g5f
+;;             [2 1])
+;;            (Real 8 [0]))
+;;           Q
+;;           [1 3724932396])
+;;          (Real 4 [])
+;;          (Integer 1 []))))
+;;       (List (Class ClassType TG GFortranModule Private [4 6]))
+;;       (Logical 8 [1 0]))
+;;      [:tuple
+;;       {:head Tuple,
+;;        :ttype
+;;        [[:derived
+;;          {:head Derived,
+;;           :derived-type
+;;           {:head DerivedType,
+;;            :symbol-table symbol-table-placeholder,
+;;            :nym y99Jg,
+;;            :members [eDC L3 Dn],
+;;            :abi Intrinsic,
+;;            :access Private,
+;;            :parent [:derived-type ANdva]},
+;;           :dims [[:bigint 2675]]}]
+;;         [:list
+;;          {:head List,
+;;           :ttype
+;;           [:set
+;;            {:head Set,
+;;             :ttype
+;;             [:tuple
+;;              {:head Tuple,
+;;               :ttype
+;;               [[:enum
+;;                 {:head Enum,
+;;                  :enum-type
+;;                  {:head EnumType,
+;;                   :symbol-table symbol-table-placeholder,
+;;                   :nym so,
+;;                   :members [G1tlT bKOn9 GS8],
+;;                   :abi BindC,
+;;                   :access Private,
+;;                   :type
+;;                   [:dict
+;;                    {:head Dict,
+;;                     :key-type
+;;                     [:derived
+;;                      {:head Derived,
+;;                       :derived-type
+;;                       {:head DerivedType,
+;;                        :symbol-table symbol-table-placeholder,
+;;                        :nym o,
+;;                        :abi GFortranModule,
+;;                        :access Private,
+;;                        :parent [:derived-type n5g5f]},
+;;                       :dims [[:nat-int 2] [:nat-int 1]]}],
+;;                     :value-type
+;;                     [:real
+;;                      {:head Real, :kind 8, :dims [[:nat-int 0]]}]}],
+;;                   :parent [:derived-type Q]},
+;;                  :dims [[:nat-int 1] [:bigint 3724932396]]}]
+;;                [:real {:head Real, :kind 4, :dims []}]
+;;                [:integer {:head Integer, :kind 1, :dims []}]]}]}]}]
+;;         [:list
+;;          {:head List,
+;;           :ttype
+;;           [:class
+;;            {:head Class,
+;;             :class-type
+;;             {:head ClassType,
+;;              :nym TG,
+;;              :abi GFortranModule,
+;;              :access Private},
+;;             :dims [[:nat-int 4] [:nat-int 6]]}]}]
+;;         [:logical
+;;          {:head Logical,
+;;           :kind 8,
+;;           :dims [[:nat-int 1] [:nat-int 0]]}]]}]]
+;;     [(Pointer (Complex 8 []))
+;;      [:pointer
+;;       {:head Pointer,
+;;        :type [:complex {:head Complex, :kind 8, :dims []}]}]]
+;;     [(TypeParameter
+;;       [0 2052028268]
+;;       Restrction
+;;       SupportsZero
+;;       Restrction
+;;       SupportsPlus
+;;       Restrction
+;;       Any
+;;       Restrction
+;;       SupportsPlus)
+;;      [:type-parameter
+;;       {:head TypeParameter,
+;;        :dims [[:nat-int 0] [:bigint 2052028268]],
+;;        :rt
+;;        [{:head Restrction, :rt SupportsZero}
+;;         {:head Restrction, :rt SupportsPlus}
+;;         {:head Restrction, :rt Any}
+;;         {:head Restrction, :rt SupportsPlus}]}]]
+;;     [(Pointer
+;;       (Enum
+;;        EnumType
+;;        symbol-table-placeholder
+;;        t0
+;;        W
+;;        Interactive
+;;        Private
+;;        (TypeParameter [])
+;;        YP
+;;        [13634627160747788 12409532]))
+;;      [:pointer
+;;       {:head Pointer,
+;;        :type
+;;        [:enum
+;;         {:head Enum,
+;;          :enum-type
+;;          {:head EnumType,
+;;           :symbol-table symbol-table-placeholder,
+;;           :nym t0,
+;;           :members [W],
+;;           :abi Interactive,
+;;           :access Private,
+;;           :type [:type-parameter {:head TypeParameter, :dims []}],
+;;           :parent [:derived-type YP]},
+;;          :dims [[:bigint 13634627160747788] [:bigint 12409532]]}]}]]
+;;     [(Logical 1 [234785814526])
+;;      [:logical
+;;       {:head Logical, :kind 1, :dims [[:bigint 234785814526]]}]]
+;;     [(CPtr) [:cptr {:head CPtr}]])
 
 ;;               _      _    _
 ;; __ ____ _ _ _(_)__ _| |__| |___
